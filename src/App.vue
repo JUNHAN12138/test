7641bfd07dd6a2c1efe9fbb1b91da4e0acf3e8fe
liangml <2206821639@qq.com>
2021-04-11 22:16:12 +0800

    测试

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-link to="/Home">Home</router-link>
+    <router-link to="/About">About</router-link>
+    <router-link to="/edit">edit</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
